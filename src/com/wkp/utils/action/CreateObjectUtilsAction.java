c0b6b6ef2aa9edc3bbfb86edd3f71acba2a6ef8e
wkp111 <1535514884@qq.com>
2017-08-10 16:44:28 +0800

    添加一些工具类

---
+++ src/com/wkp/utils/action/CreateObjectUtilsAction.java
@@ -0,0 +1,21 @@
+package com.wkp.utils.action;
+
+import com.intellij.ide.IdeView;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.psi.JavaDirectoryService;
+import com.intellij.psi.PsiDirectory;
+
+public class CreateObjectUtilsAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        IdeView ideView = e.getData(LangDataKeys.IDE_VIEW);
+        if (ideView != null) {
+            PsiDirectory psiDirectory = ideView.getOrChooseDirectory();
+            if (psiDirectory != null) {
+                JavaDirectoryService.getInstance().createClass(psiDirectory, "ObjectUtils","ObjectUtils");
+            }
+        }
+    }
+}
